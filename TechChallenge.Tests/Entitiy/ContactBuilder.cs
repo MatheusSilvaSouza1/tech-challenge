6a552680f5cffd2f2d3c3787acbbc1b5fce781e2
Filipe Silva <cloudwalstorn@gmail.com>
2024-05-21 23:02:38 -0300

    remoção de código comentado

--- TechChallenge.Tests/Entitiy/ContactBuilder.cs
+++ TechChallenge.Tests/Entitiy/ContactBuilder.cs
@@ -11,8 +11,6 @@ public class ContactBuilder
     {
         var contact = new Faker<ContactDTO>()
             .RuleFor(c => c.Name, (f) => f.Person.FullName)
-            //.RuleFor(u => u.Phone, f =>  f.Phone.PhoneNumber("!!9########")
-            //    .Replace("!", $"{f.Random.Int(min: 1, max: 9)}"))
             .RuleFor(u => u.Phone, f => string.Concat(f.Random.Int(10, 99), f.Phone.PhoneNumber("9########"))
                 .Replace("!", $"{f.Random.Int(min: 1, max: 9)}"))
             .RuleFor(c => c.Email, (f) => f.Internet.Email(f.Person.FullName));
