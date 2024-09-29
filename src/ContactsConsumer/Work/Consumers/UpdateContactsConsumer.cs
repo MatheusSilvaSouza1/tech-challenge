2be212213c116ceccd04ff198b5319e62225a34d
Tiago Lopes <saxtiago14@gmail.com>
2024-09-29 15:08:57 -0300

    feat: flow to update contact using rabbitMQ

--- src/ContactsConsumer/Work/Consumers/UpdateContactsConsumer.cs
+++ src/ContactsConsumer/Work/Consumers/UpdateContactsConsumer.cs
@@ -1,22 +1,34 @@
-﻿using Contracts;
+﻿using Application.Services;
+using Contracts;
+using Domain.DTOs;
 using MassTransit;
 
 namespace Work.Consumers
 {
     public class UpdateContactsConsumer : IConsumer<ContactUpdateMessage>
     {
+        public readonly IContactServices _contactService;
         public readonly ILogger<UpdateContactsConsumer> _logger;
 
-        public UpdateContactsConsumer(ILogger<UpdateContactsConsumer> logger)
+        public UpdateContactsConsumer(ILogger<UpdateContactsConsumer> logger, IContactServices contactServices)
         {
+            _contactService = contactServices;
             _logger = logger;
         }
 
-        public Task Consume(ConsumeContext<ContactUpdateMessage> context)
+        public async Task Consume(ConsumeContext<ContactUpdateMessage> context)
         {
-            _logger.LogInformation($"Received contact Update: {context.Message}");
+            _logger.LogInformation($"Received contact Update: {context.MessageId}");
 
-            return Task.CompletedTask;
+            await _contactService.UpdateContact(new ContactUpdateDTO()
+            {
+                Id = context.Message.Id,
+                Email = context.Message.Email,
+                Name = context.Message.Name,
+                Phone = context.Message.Phone
+            });
+
+            _logger.LogInformation("Updated Contact", context.MessageId);
         }
     }
 }
